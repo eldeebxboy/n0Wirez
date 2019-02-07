451ec21fcc47e3a559c3960b5da7f3af9fe49527
Jiri Kuba <jiri.k@ecobee.com>
2019-02-07 18:02:41 +0000

    Add pin mapping

--- thermostat_side_esp32_devkit_v1/thermostat_side_esp32_devkit_v1.ino
+++ thermostat_side_esp32_devkit_v1/thermostat_side_esp32_devkit_v1.ino
@@ -1,11 +1,23 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
-#define INPUT_PIN 34
+//#include <WiFi.h>
+//#include <HTTPClient.h>
 #define LED 2
+
+#define INPUT0 19 // D19
+#define INPUT1 21 // D21
+#define INPUT2 22 // D22
+#define INPUT3 23 // D23
+
 const char *ssid = "YOUR_SSID";
 const char *password = "password";
 void setup()
 {
+    //pinMode(INPUT_PIN, INPUT);
+    pinMode(LED, OUTPUT);
+    pinMode(INPUT0, INPUT);
+    pinMode(INPUT1, INPUT);
+    pinMode(INPUT2, INPUT);
+    pinMode(INPUT3, INPUT);
+/*
     Serial.begin(115200);
     delay(1000);		//Delay needed before calling the WiFi.begin
     pinMode(INPUT_PIN, INPUT);
@@ -16,10 +28,21 @@ void setup()
 	Serial.println("Connecting to WiFi..");
     }
     Serial.println("Connected to the WiFi network");
+*/
 }
 
 void loop()
 {
+    int input0 = digitalRead(INPUT0);
+    int input1 = digitalRead(INPUT1);
+    int input2 = digitalRead(INPUT2);
+    int input3 = digitalRead(INPUT3);
+    if (input0 |input1 |input2 | input3) {
+         digitalWrite(LED, HIGH);
+    } else {
+         digitalWrite(LED, LOW);
+    }
+/*
     int val = digitalRead(INPUT_PIN);
     Serial.println(val);
     if (val) {
@@ -47,4 +70,5 @@ void loop()
 	Serial.println("Error in WiFi connection");
     }
     delay(1000);		//Send a request every 10 seconds
+*/
 }
